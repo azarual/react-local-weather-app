ad67f8d2d5e962905a10cb8a3c99ecc3ad5ae93b
paweltar <paweltar@gmail.com>
2017-07-11 18:37:49 +0200

    Weather widget in progress

--- src/App.js
+++ src/App.js
@@ -2,8 +2,27 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import WeatherSection from "./components/WeatherSection/WeatherSection.js";
+import { getData } from "./helpers/get-position.js";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    getData();
+    this.state = {
+      isReady: false
+    };
+  }
+  componentDidMount() {
+    if (sessionStorage.coordinates !== "") {
+      this.setState({
+        isReady: true,
+        userPosition: {
+          lat: JSON.parse(sessionStorage.coordinates).lat,
+          long: JSON.parse(sessionStorage.coordinates).long
+        }
+      });
+    }
+  }
   render() {
     return (
       <div className="uk-section uk-light container--main">
@@ -17,7 +36,12 @@ class App extends Component {
 
           <div className="uk-text-center uk-grid">
             <div className="uk-width-1-3@m">
-              <WeatherSection />
+              {this.state.isReady &&
+                <WeatherSection
+                  loading={this.state.isReady}
+                  lat={this.state.userPosition.lat}
+                  long={this.state.userPosition.long}
+                />}
             </div>
           </div>
         </div>
--- src/components/WeatherSection/WeatherSection.js
+++ src/components/WeatherSection/WeatherSection.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import WeatherHeader from "../WeatherHeader/WeatherHeader.js";
 // import styles from "./styles.css.js";
+// import axios from "axios";
 
 class WeatherSection extends Component {
+  componentDidMount() {}
   render() {
     return (
       <div className="uk-card uk-card-secondary uk-box-shadow-large">
         <WeatherHeader temperature={21} />
+        <p>
+          {this.props.lat}
+        </p>
+        <p>
+          {this.props.long}
+        </p>
       </div>
     );
   }
